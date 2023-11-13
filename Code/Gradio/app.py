9c0b37569269ed1864ab1df801992c58430f7ab4
Fatih <f.fatih@outlook.fr>
2023-11-13 13:54:23 +0100

    test ecriture fichier gradio

--- Code/Gradio/app.py
+++ Code/Gradio/app.py
@@ -1,7 +1,20 @@
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+def ecrire_dans_fichier(texte):
+    nom_fichier = "sortie.txt"
+    with open(nom_fichier, "w") as fichier:
+        fichier.write(texte)
+    return f"Contenu enregistré dans {nom_fichier}"
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+iface = gr.Interface(
+    fn=ecrire_dans_fichier,
+    inputs=gr.Textbox(),
+    outputs="text",
+    live=True,
+    title="Gradio - Écrire dans un fichier",
+    description="Saisissez du texte et appuyez sur le bouton pour l'enregistrer dans un fichier texte.",
+    theme="default",
+    allow_flagging=False,
+)
+
 iface.launch()
